c492f3d3561868631e94837ad0be24a310f07956
agosfasanelli <agosfasanelli@gmail.com>
2022-12-01 08:53:06 +0000

    jhascbd

--- src/models.py
+++ src/models.py
@@ -8,26 +8,51 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Usuario(Base):
+    __tablename__ = 'usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, ForeignKey('planets.id'), ForeignKey('characters.id'), primary_key=True)
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    user_name = Column(String(250), default= {name})
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    age = Column(Integer, nullable=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, ForeignKey('favs.characters_id'), primary_key=True)
+    name = Column(String(250), nullable=False)
+    Related_Starships = Column(String(250), nullable=False)
+    Related_Vehicles = Column(String(250), nullable=False)
+    Related_Films = Column(String(250), nullable=False)
+    Homeworld = Column(String(250), nullable=False)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, ForeignKey('favs.planets_id'), primary_key=True)
+    name = Column(String(250), nullable=False)
+    Residents = Column(String(250), nullable=False)
+    Related_Films = Column(String(250), nullable=False)
+
+class Favs(Base):
+    __tablename__ = 'Favs'
+    user_id = Column(Integer, primary_key=True)
+    planets_id = Column(String(250), ForeignKey('planets.id'), nullable=False)
+    characters_id = Column(Integer, ForeignKey('characters.id'))
+    total = Column(Integer )
+    
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+try:
+    result= render_er(Base, 'diagram.png')
+    print('Success! Check the diagram.png file')
+
+except Exception as e:
+    print ('There was a problem generating the diagram')
+    raise e
