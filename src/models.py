28c018125fa4f5ddddd7a405dd152a16b6230562
agosfasanelli <agosfasanelli@gmail.com>
2022-12-20 23:47:40 +0000

    chjasbd

--- src/models.py
+++ src/models.py
@@ -12,36 +12,29 @@ class Usuario(Base):
     __tablename__ = 'usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, ForeignKey('planets.id'), ForeignKey('characters.id'), primary_key=True)
-    name = Column(String(250), nullable=False)
-    last_name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
     user_name = Column(String(250), default= {name})
     password = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
-    age = Column(Integer, nullable=True)
-
+   
 
 class Characters(Base):
     __tablename__ = 'characters'
-    id = Column(Integer, ForeignKey('favs.characters_id'), primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    Related_Starships = Column(String(250), nullable=False)
-    Related_Vehicles = Column(String(250), nullable=False)
-    Related_Films = Column(String(250), nullable=False)
     Homeworld = Column(String(250), nullable=False)
 
 class Planets(Base):
     __tablename__ = 'planets'
-    id = Column(Integer, ForeignKey('favs.planets_id'), primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     Residents = Column(String(250), nullable=False)
-    Related_Films = Column(String(250), nullable=False)
+
 
 class Favs(Base):
     __tablename__ = 'Favs'
-    user_id = Column(Integer, primary_key=True)
-    planets_id = Column(String(250), ForeignKey('planets.id'), nullable=False)
-    characters_id = Column(Integer, ForeignKey('characters.id'))
+    planets_id = Column(String(250), nullable=False)
+    characters_id = Column(Integer)
     total = Column(Integer )
     
 
